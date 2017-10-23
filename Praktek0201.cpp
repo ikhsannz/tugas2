fc263a03ae3af9226924f8c424612a0c73e01e3d
khairul ikhsan nurzeha <32305997+ikhsannz@users.noreply.github.com>
2017-10-24 07:18:15 +0800

    Perbaikan

--- Praktek0201.cpp
+++ Praktek0201.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main(){
-	nilai=nilai+1;
+		
 	int nilai;
-	int 1Nilai;
+	int Nilai1;
 	
-	cout << nilai;
-	cout << 1Nilai;
+	
+	cout<< "masukkan nilai :";
+	cin >> nilai;
+	Nilai1=nilai+1;
+	cout<<"hasil	:" << Nilai1;
 }
